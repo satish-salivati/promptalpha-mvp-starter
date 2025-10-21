7a42cb3342fb0cc4c3732ce28c0ad119e8612452
satish-salivati <salivatisatish@gmail.com>
2025-10-21 14:20:26 +0530

    Update admin.tsx

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { supabaseServer } from "@/lib/supabaseServer";
+import { supabaseServer } from "../lib/supabaseServer";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const supabase = supabaseServer();
